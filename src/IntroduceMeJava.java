b6b897afa0c044b476a00485b4a69dff4a643cd5
hmkim199 <hmkim199@gmail.com>
2024-10-28 22:09:00 +0900

    feat: 미션1-3

---
+++ src/IntroduceMeJava.java
@@ -0,0 +1,6 @@
+public class IntroduceMeJava {
+    public static void main(String[] args) {
+        System.out.println("안녕하세요. 제 블로그 닉네임은 sue 입니다. " +
+                "열심히 학습 해보겠습니다!");
+    }
+}
