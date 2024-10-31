1255cdb700b78a1cda5b3ad912df0e4929326f41
hmkim199 <hmkim199@gmail.com>
2024-10-31 19:42:17 +0900

    섹션4

---
+++ src/operator/ex/OperationEx3.java
@@ -0,0 +1,13 @@
+package operator.ex;
+
+public class OperationEx3 {
+    public static void main(String[] args) {
+        int score = 101;
+        boolean result = 80 <= score && score <=100;
+        System.out.println(result);
+
+        final int DEFAULT_LIMIT = 50;
+        System.out.println(DEFAULT_LIMIT);
+        //DEFAULT_LIMIT = 30;
+    }
+}
